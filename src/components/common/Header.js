9bb696596fd5a2fc08bf055a34fc571b2db90183
bxm0927 <80583600@qq.com>
2020-01-11 19:04:19 +0800

    fix

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import { connect } from 'react-redux'
 import { shuffle } from 'lodash'
 import styles from './Header.module.scss'
